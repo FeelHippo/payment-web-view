b774d864f05697d9498558cd36ead5f6fdf8fa4b
Dragi Kamov <dragikamov@gmail.com>
2024-01-31 13:57:01 +0100

    Additional changes

--- lib/payment.dart
+++ lib/payment.dart
@@ -1,5 +1,5 @@
-
 import 'dart:convert' as convert;
+import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:payment/widgets/webview.dart';
 import 'payment_platform_interface.dart';
@@ -7,7 +7,7 @@ import 'package:http/http.dart' as http;
 
 class PaymentWidget extends StatefulWidget {
   final String sparkProvisioningIdentifier;
-  const PaymentWidget({ Key? key, required this.sparkProvisioningIdentifier });
+  const PaymentWidget({Key? key, required this.sparkProvisioningIdentifier});
 
   @override
   _PaymentWidgetState createState() => _PaymentWidgetState();
@@ -16,18 +16,58 @@ class PaymentWidget extends StatefulWidget {
 class _PaymentWidgetState extends State<PaymentWidget> {
   late String transactionId;
 
-  // TODO: replace with GET transactionId from as-payment service
-  var url =
-      Uri.https('www.googleapis.com', '/books/v1/volumes', {'q': '{http}'});
+  Future<void> sendPostRequest() async {
+    transactionId = '240126160355738024';
+    var refno = DateTime.now().millisecondsSinceEpoch.toString();
+
+    var payload = jsonEncode({
+      'redirect': {
+        'successUrl': "successUrl",
+        'errorUrl': "errorUrl",
+        'cancelUrl': "cancelUrl"
+      },
+      'refno': refno,
+      'amount': 2000,
+      'currency': 'CHF'
+    });
+
+    var headers = {
+      'Content-Type': 'application/json',
+      'Authorization': 'Basic MTEwMDAyNTgzNTozY29mTjNNeFhhQkg3VWw4',
+      'Host': 'api.sandbox.datatrans.com',
+    };
+
+    try {
+      var response = await http.post(
+        Uri.parse('https://api.sandbox.datatrans.com/v1/transactions'),
+        headers: headers,
+        body: payload,
+      );
+
+      if (response.statusCode == 201) {
+        print('Response data: ${response.body}');
+        print(response.body);
+        transactionId = response.body;
+      } else {
+        print('Request failed with status: ${response.statusCode}.');
+      }
+    } catch (e) {
+      print('Error: $e');
+    }
+  }
 
   @override
   void initState() {
-    final response = http.get(url);
-    transactionId = '240126151215045171';
+    super.initState();
+    // final response = http.get(url);
+    // sendPostRequest();
+    transactionId = '240126161244431407';
   }
 
   @override
   Widget build(BuildContext context) {
-    return AsPaymentWebview(transactionId: transactionId,);
+    return AsPaymentWebview(
+      transactionId: transactionId,
+    );
   }
 }
